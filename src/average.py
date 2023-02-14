20c424d9a381e67a3d03bee860ef251d940a75cf
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2023-02-14 13:23:54 -0300

    exercise2:average

--- src/average.py
+++ src/average.py
@@ -1,2 +1,8 @@
-def find_average(numbers: list[int]) -> float:
-    raise NotImplementedError
+def find_average(numbers: "list[int]") -> float:
+    ## raise NotImplementedError
+    sum = 0
+    for number in numbers:
+        sum = sum + number
+
+    return sum / len(numbers)
+
